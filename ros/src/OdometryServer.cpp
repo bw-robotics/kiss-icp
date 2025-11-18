0453860d15be71fdb73d7691925bc65261a26b57
Adi Levy <adilevy15@gmail.com>
2025-11-18 12:02:14 +0200

    Added adaptive covariance to odometry messages
    - changed quality error factorization from linear to quadratic for smoother and better covariance estimation

--- ros/src/OdometryServer.cpp
+++ ros/src/OdometryServer.cpp
@@ -341,9 +341,9 @@ double OdometryServer::computeCovarianceMultiplier(size_t num_correspondences,
     quality_factor = std::clamp(quality_factor, 0.0, 1.0);
 
     // 5. Map quality [0,1] -> multiplier [1, max_covariance_multiplier_]
-    //    Qubic mapping.
+    //    Quadratic mapping. Options for Qubic, Quadratic, Linear
     const double error = 1.0 - quality_factor;      // in [0, 1]
-    double multiplier = 1.0 + (max_covariance_multiplier_ - 1.0) * std::pow(error, 3);
+    double multiplier = 1.0 + (max_covariance_multiplier_ - 1.0) * error * error;
 
     // 6. Optional exponential smoothing to reduce jitter frame-to-frame
     if (enable_covariance_smoothing_) {
